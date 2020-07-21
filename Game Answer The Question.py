174c304c8da133904e4a4d00723f4fd76181c4e0
hafizhpinandhito <68559355+hafizhpinandhito@users.noreply.github.com>
2020-07-21 07:35:16 +0700

    Add files via upload
    Hafizh Faishal Pinandhito 19.83.0358

---
+++ Game Answer The Question.py
@@ -0,0 +1,135 @@
+
+def Question1_0():
+    print("#Fakta sejarah indonesia#")
+    print("Siapakah penulis naskah pidato bahasa inggris pertama yg dipercayakan Bung Karno?: ")
+    jawaban = input()
+    if jawaban == "Bung Tomo" :
+        print("Terlalu jauh Sobat")
+        print("beliau bukan penulis melainkan pejuang dari surabaya")
+    elif jawaban == "Made Tantri" :
+        print("Hampir mendekati jawaban")
+        print("cobalah pikirkan nama panggilan anak pada suku bali")
+    elif jawaban == "Ketut Tantri" :
+        print("hmm kamu lumayan jago mari kita coba uji pertanyaan selanjutnya")
+        print("Fokus dan Konsentrasi!!!")
+    elif jawaban == "Pangeran Diponegoro" :
+        print("sayang sekali jawabanmu sangat jauh")
+        print("beliau seorang pejuang yg sama seperti bung tomo namun beda daerah perjuangan")
+    elif jawaban == "Soedirman" :
+        print("sangat sangat sangat sangat jauh")
+        print("perbanyak belajar lagi ya!!!")
+    elif jawaban == "bung tomo" :
+        print("cobalah menulis dengan awalan huruf besar")
+        print("mari lupakan soal tadi dan fokus soal selanjutnya")
+    elif jawaban == "made tantri" :
+        print("cobalah menulis dengan awalan huruf besar")
+        print("mari lupakan soal tadi dan fokus soal selanjutnya")
+    elif jawaban == "ketut tantri" :
+        print("cobalah menulis dengan awalan huruf besar")
+        print("mari lupakan soal tadi dan fokus soal selanjutnya")
+    elif jawaban == "pangeran diponegoro" :
+        print("cobalah menulis dengan awalan huruf besar")
+        print("mari lupakan soal tadi dan fokus soal selanjutnya")
+    elif jawaban == "soedirman" :
+        print("cobalah menulis dengan awalan huruf besar")
+        print("mari lupakan soal tadi dan fokus soal selanjutnya")
+    else:
+        print("Banyakin belajar lagi ya!!!!!!!!!!!")
+        print("mari lupakan soal tadi dan fokus soal selanjutnya")
+
+Question1_0()
+
+def Question2_0():
+    print("#Fakta COVID19#")
+    print("Berapa Harikah Isolasi Mandiri Bagi Pendatang Dari Kota Zona Merah?: ")
+    jawaban = input()
+    if jawaban == "10" :
+        print("Sedikit Lagi Sobat")
+    elif jawaban == "13" :
+        print("Aduuuh tipis sekali kawan")
+    elif jawaban == "14" :
+        print("Wah kamu mulai jago")
+        print("mari ke soal selanjutnya supaya lebih jago")
+    elif jawaban == "15" :
+        print("oopssss kelebihan kawan")
+    elif jawaban == "17" :
+        print("cobalah lain kali ya sob")
+    elif jawaban == "19" :
+        print("sayang sekali Sudah terlampau jauh kawan")
+    else:
+        print("Banyakin belajar lagi ya!!!!!!!!!!!")
+        print("mari lupakan soal tadi dan fokus soal selanjutnya")
+
+Question2_0()
+
+def Question3_0():
+    print("Fakta COVID19")
+    print("Ingat Kawan soal hanya sekali dan tak ada clu jika salah so pikirkan baik-baik ya")
+    print("=====Focus-Focus Trula-la=======")
+    print("Berapa Hari kah lama Virus hidup pada permukaan?: ")
+    jawaban = input()
+    if jawaban == "3" :
+        print("Wah kamu mulai jago")
+        print("mari ke soal selanjutnya supaya lebih jago")
+    elif jawaban == "2" :
+        print("Aduuuh tipis sekali kawan")
+    elif jawaban == "5" :
+        print("oopssss kelebihan kawan")
+    elif jawaban == "7" :
+        print("oopssss kelebihan lumayan jauh kawan")
+    elif jawaban == "9" :
+        print("cobalah lain kali ya sob")
+    elif jawaban == "10" :
+        print("sayang sekali Sudah terlampau jauh kawan")
+    else:
+        print("Banyakin belajar lagi ya!!!!!!!!!!!")
+        print("mari lupakan soal tadi dan fokus soal selanjutnya")
+
+Question3_0()
+
+print("==========INI BATAS SOAL============")
+
+
+def Question4_0():
+    print("#Fakta COVID19#")
+    print("Daerah manakah yg pertamakali ditemukan Virus Covid19?: ")
+    jawaban = input()
+    if jawaban == "China" :
+        print("Wah kamu jago")
+        print("mari ke soal selanjutnya supaya jadi jagoan")
+    elif jawaban == "Jepang" :
+        print("Aduuuh sebelahnya kawan")
+    elif jawaban == "Amerika" :
+        print("oopssss kejauhan kawan")
+    elif jawaban == "Afrika" :
+        print("oopssss kejauhan lewat benua kawan")
+    elif jawaban == "Australia" :
+        print("Terlalu jauh di ujung dunia sob")
+    elif jawaban == "Italia" :
+        print("sayang sekali Sudah terlampau jauh kawan")
+    else:
+        print("Banyakin belajar lagi ya!!!!!!!!!!!")
+        print("Next merupakan Soal Terakhir dan Last Chance")
+
+Question4_0()
+
+def Question5_0():
+    print("#IQ test#")
+    print("pada sebuah percobaan terdapat 2 buah lampu,lampu A di coba hidup matikan selama 1 jam sedangkan Lampu B di coba hidupkan terus selama satu jam")
+    print("Menurut kamu lampu manakah yg pertama mati??: ")
+    jawaban = input()
+    if jawaban == "A" :
+        print("Selamat Kamu salah jawab")
+    elif jawaban == "B" :
+        print("Aduuuh next time ya sob")
+    elif jawaban == "Tidak Ada" :
+        print("wahhh kamu jagoan")
+    elif jawaban == "A Dan B" :
+        print("Coba Renungkan dan Ratapi ya kawan")
+    elif jawaban == "Keduanya" :
+        print("Cobalah liat pemandangan hijau agar pikiranmu fresh")
+    else:
+        print("Cobalah kamu Baca soal dan peraturan lalu Cerna serta Resapi secara Halus")
+        print("SEE YOUUU SOBAT")
+
+Question5_0()
